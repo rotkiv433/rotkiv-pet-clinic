6ce10b8af75f2fe4d70e439cdabce60ca5b84c54
Viktor Ilievski <viktor.ilievski@students.finki.ukim.mk>
2020-07-24 19:35:49 +0200

    Initial Project Add

---
+++ src/main/java/rotkiv/springframework/rotkivpetclinic/RotkivPetClinicApplication.java
@@ -0,0 +1,13 @@
+package rotkiv.springframework.rotkivpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RotkivPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RotkivPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rotkiv/springframework/rotkivpetclinic/RotkivPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package rotkiv.springframework.rotkivpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RotkivPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
