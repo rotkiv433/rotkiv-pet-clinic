951335f5147ff2d80d404df438a17283eb6fc598
Viktor Ilievski <viktor.ilievski@students.finki.ukim.mk>
2020-08-01 16:40:34 +0200

    Adding POJOs. Closes #2.

---
+++ src/main/java/rotkiv/springframework/rotkivpetclinic/model/Pet.java
@@ -0,0 +1,33 @@
+package rotkiv.springframework.rotkivpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+    private Owner owner;
+    private PetType petType;
+    private LocalDate birthday;
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+}
